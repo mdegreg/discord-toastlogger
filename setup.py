7c863017bd687a06c63a5c60c53c6efca80d6b0e
Matthew DeGregorio <mdegregorio01@gmail.com>
2017-09-11 21:35:53 -0500

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='discord-toastlogger',
+    version='0.1.0',
+    scripts=['toastbot'],
+    url='https://github.com/mdegreg/discord-toastlogger',
+    license='MIT',
+    install_requires=[
+        'discord'
+    ]
+)
